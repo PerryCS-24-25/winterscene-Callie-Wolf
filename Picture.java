99a2f1caf2d534d7f1bc133493f227648fdea325
Callie-Wolf <archocaren@gmail.com>
2024-12-13 10:26:52 -0700

    lights add, finish others

--- Picture.java
+++ Picture.java
@@ -16,6 +16,7 @@ public class Picture
     //private int dx = 5; // Speed in x direction for the sample ball
    // private int dy = -5; // Speed in y direction for the sample ball
     private Circle[] storm;
+    private Triangle[] lights;
     
     
     public Picture()
@@ -27,17 +28,36 @@ public class Picture
         
         // Turn off automatic redrawing
         pic.pause(true);
-        
-       storm= new Circle [400];
+
+        Rect shed= new Rect(0, 450, 200, 150,"#C23B22", true);
+        Rect roof= new Rect(0, 450, 200, 30, "#c4a484", true);
+        Triangle tree= new Triangle(400,150, 300, 450, "#acd8a7",true);
+       
+        storm= new Circle [400];
        for(int i=0; i<storm.length; i++)
        {
 
-             int y=0;
+             //int y=0;
              int randomX= (int)(Math.random()*800)+0;
              
              int randomY= (int)(Math.random()*600)+0;
         
             storm[i]= new Circle(randomX, randomY, 10, "#FAF9F6",true);
+        
+
+           
+       }
+
+         lights= new Triangle [50];
+       for(int i=0; i<lights.length; i++)
+       {
+
+             //int y=0;
+             int randomX= (int)(Math.random()*260)+260;
+             
+             int randomY= (int)(Math.random()*400)+200;
+        
+            lights [i]= new Triangle (randomX, randomY, 10, 20, "#87CEEB",true);
         
 
             
@@ -62,17 +82,28 @@ public class Picture
             int stormer= (int)(Math.random()*50)+0;
             storm[i].setX(storm[i].getX() + stormer);
             storm[i].setY(storm[i].getY() + stormer);
+            int xS= storm[i].getX();
+            int yS= storm[i].getY();
 
-            int valuex= storm[i].setX(storm[i].getX() + stormer);
-            int value y=storm[i].setY(storm[i].getY() + stormer);
+            int numx= (int)(Math.random()*800)+0;
 
-            if(storm[i]>800 || storm[i]>600)
+           
+
+            if(xS>=800 || yS>=600)
             {
-
+                storm[i].setX(numx);
+                storm[i].setY(0);
             }
             
         }
         
+        for(int i=0; i<lights.length; i++)
+        {
+            String color= "#ADD8E6"
+            lights[i].changeColor("#ADD8E6");
+            lights[i].sleep(1000);
+            lights[i].changeColor("#87CEEB");
+        }
  
         
         // Update the screen
